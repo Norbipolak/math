1cccf7143c8f1188ddc81b36574dfce73de30ede
Norbipolak <142392455+Norbipolak@users.noreply.github.com>
2023-09-05 10:41:16 +0200

    update hello player

---
+++ index.js
@@ -0,0 +1,45 @@
+function sayHello(player) {
+    return 'hello ' + player
+}
+
+const helloJoe = sayHello('Jake')
+const helloJohn = sayHello('John')
+const helloJane = sayHello('Jane')
+
+console.log(helloJoe)
+console.log(helloJohn)
+console.log(helloJane)
+
+// function sayHello(player) {
+//     return 'hello ' + player
+// }
+
+// console.log(sayHello('Jake'))
+// console.log(sayHello('John'))
+// console.log(sayHello('Jane'))
+
+// function sayHello(player) {
+//     console.log('hello ' + player)
+//     // return undefined
+// }
+
+// console.log(sayHello('Jake'))
+// console.log(sayHello('John'))
+// console.log(sayHello('Jane'))
+
+// function sayHello(player){
+// console.log('hello ' + player)
+// }
+
+// sayHello('Jake')
+// sayHello('John')
+// sayHello('Jane')
+
+// const player = 'Joe'
+// function sayHello (){
+//    console.log('hello ' + player)
+//}
+//sayHello()
+
+// const player = process.argv[2]
+// console.log('hello ' + player)
